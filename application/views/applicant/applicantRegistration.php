2a62ba66c90c2ec0dd7547c340ae7af24a3fdde6
yanggskie <lyahbianca.aquino04@gmail.com>
2021-12-13 01:04:09 +0800

    Applicant Frontend sidebar

---
+++ application/views/applicant/applicantRegistration.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat'>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Poppins'>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/applicant.css'); ?>" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="d-flex justify-content-between text-white header px-3 py-2">
+        <div class="brand">
+            Technological University of the Philippines
+        </div>
+        <!-- <div class="cancel">
+            Cancel Application
+        </div> -->
+    </div>
+    <div class="container-fluid overflow-hidden">
+        <div class="row vh-100">
+            <div class="col-lg-3 col-md-3 col-sm-0 col-xl-2 px-0 bg-default d-flex sticky-top">
+                <div class="d-flex flex-md-column flex-row flex-grow-1 align-items-center text-dark">
+                    <div href="/" class="d-block align-items-center pb-sm-3 text-dark text-decoration-none">
+                        <div class="d-none d-md-inline">
+                            <img src="assets/images/applicantAvatar.svg" alt="" class="pt-4 mx-4"styles="width:1rem">
+                            <br>
+                            <p class="text-center pt-4 h5">Welcome,</p>
+                            <p class="text-center pt-2 fw-bold h5">Applicant-21-2322 </p>
+                            <hr>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Contents -->
+            <div class="col d-flex flex-column h-sm-100">
+            <div class="mt-3 applicant-head text-white px-3"> 
+                    Appicant ID: <span class="fw-bold"> Applicant-21-2123 </span> <!--APPLICANT NUMBER READ -->
+                </div>
+
+                <!--Information -->
+                <div class=" mt-3 applicant-head text-dark px-3 information"> 
+                   <div class="row">
+                       <div class="col-1 float-left">
+                           <i class="fa fa-info-circle" aria-hidden="true"></i>
+                       </div>
+                       <div class="col-11">
+                       1. Kindly type 'NA' in boxes where there are no possible answers to the information being requested. <br>
+                       2. Please enter a valid email address to receive the test permit.
+                       </div>
+                   </div>
+                </div>
+                <main class="row">
+                    <div class="col pt-4">
+                        <h3>Vertical Sidebar that switches to Horizontal Navbar</h3>
+                        <p class="lead">An example multi-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single menu is be open at a time.</p>
+                        <hr />
+                          <h3>More content...</h3>
+                        <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+                        <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies, forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
+                        </div>
+                </main>
+                    <footer class="row bg-light py-4 mt-auto">
+                        <div class="col"> Footer content here... </div>
+                    </footer>
+                </div>
+        </div>
+    </div>
+
+</body>
+</html>
