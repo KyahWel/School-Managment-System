3d0b59727f6155170350025afa74ba446cd97aab
Kimberleeeey <kimdelgado215@gmail.com>
2022-01-12 22:01:19 +0800

    Added Faculty My Students

---
+++ assets/js/facultyMyStudents.js
@@ -0,0 +1,9 @@
+function mainPage(){
+    document.getElementById('mainPage').style.display = "block";
+    document.getElementById('sectionPage').style.display = "none";
+}
+
+function sectionPage(){
+    document.getElementById('mainPage').style.display = "none";
+    document.getElementById('sectionPage').style.display = "block";
+}
